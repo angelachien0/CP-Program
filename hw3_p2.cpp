62745522f9cc71a59689b02fc6944527856cae33
angelachien0 <angela.xr.chien@gmail.com>
2025-04-22 18:42:59 +0800

    加入hw3_p2.cpp 和 hw3_p2.exe

---
+++ hw3_p2.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tourist = 3858;
+    int ksun48 = 3679;
+    int Benq = 3658;
+    int Um_nik = 3648;
+    int apiad = 3638;
+    int Stonefeang = 3630;
+    int ecnerwala = 3613;
+    int mnbvmar = 3555;
+    int newbiedmy = 3516;
+    int semiexp = 3481;
+
+    string n;
+    cin >> n;
+
+    if(n == "tourist")
+        cout << tourist;
+    else if( n == "ksun48")
+        cout << ksun48;
+    else if( n == "Benq")
+        cout << Benq;
+    else if( n == "Um_nik")
+        cout << Um_nik;
+    else if( n == "apiad")
+        cout << apiad;
+    else if( n == "Stonefeang")
+        cout << Stonefeang;
+    else if( n == "ecnerwala")
+        cout << ecnerwala;
+    else if( n == "mnbvmar")
+        cout << mnbvmar;
+    else if( n == "newbiedmy")
+        cout << newbiedmy;
+    else if( n == "semiexp")
+        cout << semiexp;
+}
